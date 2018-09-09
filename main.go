51237daf4cef98790f08bc13a54b07e2070dcb11
kminhc <kminhc@gmail.com>
2018-09-09 15:34:18 +0200

    added a HTTP Server

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/kminhc/goblog/accountservice/service"
 )
 
 var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	service.StartWebServer("6767")
 }
